3314a67368ff015831211dfb4c5fc3a41fe21b95
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-11-04 16:46:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import gym
+import numpy as np
+
+
+def run_episode(env, theta, render=False):
+	"""
+	Runs a single episode given env and weight vector.
+	"""
+	observation = env.reset()
+	done = False
+	total_reward = 0
+	while not done:
+		if render: env.render()
+		action = np.dot(observation, theta.reshape(-1, 1)).flatten()[0] > 0
+		# action = env.action_space.sample()  # take a random action
+		observation, reward, done, _ = env.step(action)
+		total_reward += reward
+	return total_reward
+
+def evaluate(env, get_theta, n_episodes=10**4, max_reward=200):
+	"""
+	:param env: gym environment
+	:param get_theta: random weight generating function (eg. gaussian or uniform)
+	:param n_episodes: how many times should we keep searching for max
+	:param max_reward: Target reward
+	:return: Number of iterations after which max_reward has been achieved for the first time.
+	"""
+	for i in range(n_episodes):
+		theta = get_theta()
+		reward = run_episode(env, theta, render=False)
+		if reward == max_reward:  # maximum reward in cartpole = 200. Env stops after reaching that
+			return i
+	return None
+
+
+def main():
+	env = gym.make('CartPole-v0')
+	uniform_scores = []
+	normal_scores = []
+
+	uniform = lambda: np.random.random(4) * 2 - 1
+	normal = lambda: np.random.randn(4) * 2 - 1
+
+	for _ in range(100):
+		uniform_scores.append(evaluate(env, uniform))
+		normal_scores.append(evaluate(env, normal))
+	env.close()
+
+	print('Avg. (out of 100) number of episodes after which return = 200 has been achieved for randomly generated weights:')
+	print('UNIFORM distribution: {}'.format(np.mean(uniform_scores)))
+	print('NORMAL distribution: {}'.format(np.mean(normal_scores)))
+
+
+if __name__ == '__main__':
+	main()
