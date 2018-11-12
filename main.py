743402d55282ecc7b3a4274ddeba5e23bc26fb09
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-11-12 18:07:01 +0100

    Uncommenting - fix

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from hill_climbing import run_hill_climbing
 from plotting import histograms
 
 def main():
-	# print('Random search:')
-	# results = run_random_search(num_runs=1000)
-	# histograms(results, 'random_search')
+	print('Random search:')
+	results = run_random_search(num_runs=1000)
+	histograms(results, 'random_search')
 
 	learn_rate = 0.1
 	print('\nHill climbing (learn_rate={}):'.format(learn_rate))
