5fbe1c664fb9248c965981a45496ebf7482ab6fa
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-11-14 07:58:31 +0100

    Adding dependencies for the correct spinup (spinningup)

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,3 +1,11 @@
 import tensorflow as tf
-import spinup
+import gym
+import spinningup as spinup
 
+def main():
+	env_fn = lambda: gym.make('CartPole-v0')
+	spinup.vpg(env_fn)
+
+
+if __name__ == '__main__':
+	main()
