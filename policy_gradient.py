029c26457e6f524a1b49cc3dbc0997dd6497b996
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-11-23 08:13:09 +0100

    Making spinup.vpg work - WIP

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,11 +1,11 @@
+# from spinningup.spinup.algos.vpg import vpg
+from spinup import vpg  # fixme - there's something wrong with this, doesn't import for w/e reason
 import tensorflow as tf
 import gym
-import spinningup as spinup
-
-def main():
-	env_fn = lambda: gym.make('CartPole-v0')
-	spinup.vpg(env_fn)
 
 
-if __name__ == '__main__':
-	main()
+def env_fn(): gym.make('CartPole-v0')
+
+
+ac_kwargs = dict(hidden_sizes=[64, 64], activation=tf.nn.relu)
+vpg(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=100)
