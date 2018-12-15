fc68a24602687094d998a0153b174789258fe5e0
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-12-15 09:03:10 +0100

    Adding policy gradient

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from random_search import run_random_search
 from hill_climbing import run_hill_climbing
-from plotting import histograms
+from policy_gradient import run_policy_gradient
+from plotting import histograms, histogram
 
 
 def main():
-	print('Random search:')
-	results = run_random_search(num_runs=1000)
-	histograms(results, 'random_search')
+	# print('Random search:')
+	# results = run_random_search(num_runs=1000)
+	# histograms(results, 'random_search')
+	#
+	# learn_rate = 0.1
+	# print('\nHill climbing (learn_rate={}):'.format(learn_rate))
+	# results = run_hill_climbing(learn_rate=learn_rate, num_runs=100)
+	# histograms(results, 'hill_climbing', fix_axes=False)
 
-	learn_rate = 0.1
-	print('\nHill climbing (learn_rate={}):'.format(learn_rate))
-	results = run_hill_climbing(learn_rate=learn_rate, num_runs=100)
-	histograms(results, 'hill_climbing', fix_axes=False)
+	print('Policy gradient:')
+	results = run_policy_gradient(num_runs=100)
+	histogram(results, 'policy_gradient', 'Policy gradient')
 
 
 if __name__ == '__main__':
